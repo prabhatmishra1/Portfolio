5dcfb494e98f2d6aa1540f64f889d1d32a7e277d
prabhatmishra1 <mishraa.prabhat@gmail.com>
2022-09-01 15:59:49 +0530

    updated settings file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATIC_DIR=os.path.join(BASE_DIR,'static_cdn')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -121,8 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS=[
-                STATIC_DIR,
+    STATIC_DIR,
 ]
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
